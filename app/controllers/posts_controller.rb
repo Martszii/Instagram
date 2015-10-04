b9f22c6cff32d6db124e952a8543676b08aada68
Martszii <martsziiii@hotmail.hu>
2015-10-04 18:11:04 +0200

    Show Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   def new
     @post = Post.new
   end
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path  
+    redirect_to posts_path
   end
   private
 
